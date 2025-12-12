3ded7cb40ae2db0c8450db259edf35f80dea422b
ZygimantasVidmantas <zygimantas.vidmantas@mif.stud.vu.lt>
2025-12-12 22:20:15 +0200

    Update ItemRequest.cs

--- backend/Orders/Requests/ItemRequest.cs
+++ backend/Orders/Requests/ItemRequest.cs
@@ -4,10 +4,10 @@ namespace backend.Orders;
 
 public class ItemRequest
 {
-    public int? ProductId { get; set; }
-    public IEnumerable<int>? VariationIds { get; set; } = [];
-    public Currency? Currency { get; set; }
-    public int? Quantity { get; set; }
-    public decimal? Discount { get; set; }
-    public decimal? VATPercentage { get; set; }
+    public int ProductId { get; set; }
+    public IEnumerable<int> VariationIds { get; set; } = [];
+    public Currency Currency { get; set; }
+    public int Quantity { get; set; }
+    public decimal Discount { get; set; }
+    public decimal VATPercentage { get; set; }
 }
