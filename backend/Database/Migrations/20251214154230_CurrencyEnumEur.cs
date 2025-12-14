ec0969989403aace400ec9a186c236708b8b8ed4
Emil Duko <emil.duko@gmail.com>
2025-12-14 17:44:07 +0200

    Currency in Product uses Enum, Enum is Eur.

---
+++ backend/Database/Migrations/20251214154230_CurrencyEnumEur.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CurrencyEnumEur : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Currency",
+                value: "Eur");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "Currency",
+                value: "Eur");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "Currency",
+                value: "Eur");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "Currency",
+                value: "eur");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "Currency",
+                value: "eur");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "Currency",
+                value: "eur");
+        }
+    }
+}
